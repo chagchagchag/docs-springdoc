553dfd649f9b323650816d9c49a282dc9f701890
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-25 15:11:38 +0900

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Docs Springdoc</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-springdoc',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-springdoc',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
